29b28599a19397b99289b1dc57f8124cd0546452
chris <104324755+cjurgens17@users.noreply.github.com>
2022-08-03 16:10:42 -0400

    update

---
+++ Project2/src/main/java/com/revature/Project2/Project2Application.java
@@ -0,0 +1,13 @@
+package com.revature.Project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2Application.class, args);
+	}
+
+}
---
+++ Project2/src/test/java/com/revature/Project2/Project2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.Project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
