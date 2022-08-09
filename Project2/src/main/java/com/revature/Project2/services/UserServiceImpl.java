60506fb0e8b75712fa05f4e253bdc6cebe35855d
msinnemaki <matt.sinnemaki@gmail.com>
2022-08-09 15:16:34 -0600

    updating cart

--- Project2/src/main/java/com/revature/Project2/services/UserServiceImpl.java
+++ Project2/src/main/java/com/revature/Project2/services/UserServiceImpl.java
@@ -26,11 +26,8 @@ public class UserServiceImpl implements UserService {
         return (primaryKey > 0) ? true: false;
     }
 
-    @Override
-    public User getUserByUsernamePassword(String username, String password) {
-        System.out.println("Look Here: " + username + " with a password of:" + password);
-        return userRepo.findByUsernamePassword(username, password);
-    }
+
+
 
     @Override
     public User getUserByUsername(String username, String password) {
